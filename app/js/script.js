f8c4589ad3e5a1bb2b5973e5bbe8c5ca6e476db6
Rachel Chen <rachelchen110@gmail.com>
2022-09-25 09:30:57 -0700

    menu fades in and out with overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,21 +1,28 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function() {
     console.log('click hamburger');
     if (header.classList.contains('open')) { // close ham menu
         header.classList.remove('open');
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
+        fadeElems.forEach(function(element) {
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        });
+        
 
 
 
     }
     else { // open ham menu
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeElems.forEach(function(element) {
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
+        
 
     }
 });
