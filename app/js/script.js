7d06391f6c93b84adb2a6486833b0df235bdc908
Rachel Chen <rachelchen110@gmail.com>
2022-09-24 18:46:10 -0700

    fade-out overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,21 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function() {
     console.log('click hamburger');
-    if (header.classList.contains('open')) {
+    if (header.classList.contains('open')) { // close ham menu
         header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+
+
 
     }
-    else {
+    else { // open ham menu
         header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+
     }
 });
